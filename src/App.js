5f14dde7338300e600958308c4934f86d8a911ff
Abolfazl Ghorbani <ab.ghorbani28@gmail.com>
2022-08-26 13:07:08 +0200

    Create-Basic-Sleider with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import data from './data';
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 function App() {
+
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+  useEffect(()=>{
+    let lastIndex = people.length - 1;
+    if(index < 0){
+      setIndex(lastIndex)
+    }
+    if(index> lastIndex){
+      setIndex(0)
+    }
+  }, [index,people])
+
+
+  useEffect(()=>{
+   let slider = setInterval(()=>{
+      setIndex(index+1);
+    }, 3000)
+    return ()=> clearInterval(slider)
+  }, [index])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="section">
+      <div className="title">
+        <span>Customers comments</span>
+      </div>
+      <div className="section-center">
+        {
+          people.map((person, personIndex)=> {
+            const {id,image,name,title,qoute} = person;
+
+            let position = "nextSlide";
+            if(personIndex === index){
+              position = "activeSlide"
+            }
+            if(personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)) {
+              position = "lastSlide"
+            }
+
+            return(
+             <article className={position} key={id}>
+                <img src={image} alt={name} className="person-img" />
+                <h4>{name}</h4>
+                <p className="title">{title}</p>
+                <p className="qoute">{qoute}</p>
+             </article>
+            )
+          })
+        }
+
+        <button className="next" onClick={()=> setIndex(index+1)}>
+          <FiChevronRight />
+        </button>
+        <button className="prev" onClick={()=> setIndex(index-1)}>
+          <FiChevronLeft />
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,27 @@
+const people = [
+     {
+         id: 1,
+         image: './images/1.jpg',
+         name: 'Romano Ranieri',
+         title: ' Construction Engineer',
+         qoute: 'Lorem Ipsum is a fictitious text with the production of incomprehensible simplicity from the printing industry and using graphic designers, printers and texts, but also newspapers and magazines in columns and lines as necessary Lorem Ipsom is a fictitious text with the production of incomprehensible simplicity from the printing industry and using graphic designers Printers and texts, but newspapers and magazines in columns and rows as necessary.'
+     },
+      {
+         id: 2,
+         image: './images/2.jpg',
+         name: 'Sebastian Farenci',
+         title: 'web designer',
+         qoute: 'Lorem Ipsum is a fictitious text with the production of incomprehensible simplicity from the printing industry and using graphic designers, printers and texts, but also newspapers and magazines in columns and lines as necessary Lorem Ipsom is a fictitious text with the production of incomprehensible simplicity from the printing industry and using graphic designers Printers and texts, but newspapers and magazines in columns and rows as necessary.'
+        },
+    
+     {
+         id: 3,
+         image: './images/3.jpg',
+         name: 'Ronaldo',
+         title: 'mechanic',
+         qoute: 'Lorem Ipsum is a fictitious text with the production of incomprehensible simplicity from the printing industry and using graphic designers, printers and texts, but also newspapers and magazines in columns and lines as necessary Lorem Ipsom is a fictitious text with the production of incomprehensible simplicity from the printing industry and using graphic designers Printers and texts, but newspapers and magazines in columns and rows as necessary.'
+        },
+     
+ ]
+ 
+ export default people;
